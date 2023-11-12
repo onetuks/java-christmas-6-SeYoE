3d08cc191990c4f9a16700bcb82897207a874eea
SeYoE <onetuks@naver.com>
2023-11-12 20:09:35 +0900

    Feat: 주말 할인 이벤트 구현

---
+++ src/main/java/christmas/event/handler/WeekEndDayEventHandler.java
@@ -0,0 +1,42 @@
+package christmas.event.handler;
+
+import static christmas.order.MenuType.MAIN_DISH;
+
+import christmas.event.EventHandler;
+import christmas.order.Order;
+import christmas.order.VisitDate;
+import java.util.List;
+
+public class WeekEndDayEventHandler implements EventHandler {
+
+    private static final int MINIMUM_PRICE = 10_000;
+
+    private static final int DISCOUNT_PRICE = 2_023;
+
+    private static final List<Integer> EVENT_DAYS = List.of(1, 2, 8, 9, 15, 16, 22, 23, 29, 30);
+
+    @Override
+    public boolean supports(final Order order, final VisitDate visitDate) {
+        return supportMinimumPrice(order) && supportVisitDate(visitDate);
+    }
+
+    @Override
+    public boolean supportMinimumPrice(final Order order) {
+        return order.getTotalPrice() >= MINIMUM_PRICE;
+    }
+
+    @Override
+    public boolean supportVisitDate(final VisitDate visitDate) {
+        return EVENT_DAYS.contains(visitDate.getDate());
+    }
+
+    @Override
+    public int apply(final Order order, final VisitDate visitDate) {
+        return order.getOrderHistory()
+                .entrySet()
+                .stream()
+                .filter(entry -> entry.getKey().getMenuType() == MAIN_DISH)
+                .mapToInt(entry -> entry.getValue().getAmount() * DISCOUNT_PRICE)
+                .sum();
+    }
+}
---
+++ src/test/java/christmas/event/handler/WeekEndDayEventHandlerTest.java
@@ -0,0 +1,101 @@
+package christmas.event.handler;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+import christmas.order.Order;
+import christmas.order.VisitDate;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class WeekEndDayEventHandlerTest {
+
+    private WeekEndDayEventHandler eventHandler;
+
+    @BeforeEach
+    void setUp() {
+        eventHandler = new WeekEndDayEventHandler();
+    }
+
+    @Test
+    @DisplayName("총 주문 금액이 만 원이 넘지 않은 주문으로는 이벤트 적용되지 않는다.")
+    void supportMinimumPrice_LessThanMinimumPrice_False() {
+        // Given
+        final Order order = new Order("양송이수프-1");
+
+        // When
+        final boolean result = eventHandler.supportMinimumPrice(order);
+
+        // Then
+        assertThat(result).isFalse();
+    }
+
+    @Test
+    @DisplayName("총주문 금액이 만 원 이상인 주문은 이벤트 적용된다.")
+    void supportMinimumPrice_GreaterThanMinimumPrice_True() {
+        // Given
+        final Order order = new Order("티본스테이크-3, 초코케이크-1");
+
+        // When
+        final boolean result = eventHandler.supportMinimumPrice(order);
+
+        // Then
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    @DisplayName("방문 날짜가 주말이 아니라면 이벤트 적용된다.")
+    void supportVisitDate_NotWeekEndVisitDate_False() {
+        // Given
+        final VisitDate visitDate = new VisitDate("4");
+
+        // When
+        final boolean result = eventHandler.supportVisitDate(visitDate);
+
+        // Then
+        assertThat(result).isFalse();
+    }
+
+    @Test
+    @DisplayName("방문 날짜가 주말이라면 이벤트 적용된다.")
+    void supportVisitDate_WeekEndVisitDate_True() {
+        // Given
+        final VisitDate visitDate = new VisitDate("2");
+
+        // When
+        final boolean result = eventHandler.supportVisitDate(visitDate);
+
+        // Then
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    @DisplayName("방문 날짜가 주말이고, 총주문 금액이 만 원 이상이면 이벤트 적용된다.")
+    void supports_BeforeChristmasAndGreaterThanMinimumPrice_True() {
+        // Given
+        final VisitDate visitDate = new VisitDate("2");
+        final Order order = new Order("티본스테이크-3, 초코케이크-1");
+
+        // When
+        final boolean result = eventHandler.supports(order, visitDate);
+
+        // Then
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    @DisplayName("이벤트 적용 시 할인 금액을 구한다.")
+    void apply_OrderAndVisitDate_DiscountPrice() {
+        // Given
+        final VisitDate visitDate = new VisitDate("4");
+        final Order order = new Order("티본스테이크-3, 초코케이크-1");
+
+        // When
+        final int result = eventHandler.apply(order, visitDate);
+
+        // Then
+        assertThat(result).isGreaterThan(1_000);
+    }
+
+}
